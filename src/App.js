5e58521517e7a4b3dae7a08aa027a1299872f65d
Abenezer Teshome <abenezerteshome7d@gmail.com>
2024-11-02 00:47:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
+const App = () => {
+    const [joke, setJoke] = useState('')
+    const fetchDadJoke = async () => {
+      try {
+        const response = await axios.get('https://icanhazdadjoke.com/', {
+          headers: {
+            Accept: 'application/json',
+          },
+        });
+        console.log(response.data);
+        setJoke(response.data.joke);
+      } catch (error) {
+        console.error('Error fetching dad joke:', error.response);
+      }
+    };
+
+  useEffect(() => {
+   
+    fetchDadJoke();
+  }, []);
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+       <div className='card'>
+         <p className='joke'> {joke}</p>
+         <br/> 
+         <button className='generateBtn' onClick={fetchDadJoke}>Give me some Dad Joke</button>
+        </div>
+       
     </div>
-  );
-}
+  )
+};
 
-export default App;
+
+    
+export default App
